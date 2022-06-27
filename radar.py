d899408fd07c469b68a2d82228cd524e17a49c05
LevitatingBusinessMan <public@reinfernhout.xyz>
2022-06-27 22:07:37 +0200

    radar: make size and radius of radar dynamic

--- radar.py
+++ radar.py
@@ -7,6 +7,7 @@ pygame.display.set_caption('Bermuda radar')
 green = (0,255,0)
 
 size = [800,600]
+radar_radius = 250
 screen = pygame.display.set_mode(size)
 
 clock = pygame.time.Clock()
@@ -25,20 +26,20 @@ def draw():
 
 		for i in range(trail_length * line_multiplier):
 			pos = current_scan_pos
-			x = math.cos(math.radians(pos-(i/line_multiplier))) * 250 + 400
-			y = math.sin(math.radians(pos-(i/line_multiplier))) * 250 + 300
+			x = math.cos(math.radians(pos-(i/line_multiplier))) * radar_radius + size[0]/2
+			y = math.sin(math.radians(pos-(i/line_multiplier))) * radar_radius + size[1]/2
 
-			pygame.draw.line(screen,(0,(1-i/(trail_length * line_multiplier))*max_green,0),[400,300],[x,y],thickness)
+			pygame.draw.line(screen,(0,(1-i/(trail_length * line_multiplier))*max_green,0),[size[0]/2,size[1]/2],[x,y],thickness)
 
 		#draw line
-		x = math.cos(math.radians(current_scan_pos)) * 250 + 400
-		y = math.sin(math.radians(current_scan_pos)) * 250 + 300
+		x = math.cos(math.radians(current_scan_pos)) * radar_radius + size[0]/2
+		y = math.sin(math.radians(current_scan_pos)) * radar_radius + size[1]/2
 
-		#pygame.draw.aaline(screen,green,[400,300],[x,y],3)
-		pygame.draw.line(screen,green,[400,300],[x,y],3)
+		#pygame.draw.aaline(screen,green,[size[0]/2,size[1]/2],[x,y],3)
+		pygame.draw.line(screen,green,[size[0]/2,size[1]/2],[x,y],3)
 
 		# draw circle
-		pygame.draw.circle(screen,green,[400,300],250, 3)
+		pygame.draw.circle(screen,green,[size[0]/2,size[1]/2],radar_radius, 3)
 
 #Main loop
 while True:
